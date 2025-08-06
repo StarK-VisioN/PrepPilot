53c8b40c9d486ecb12d5039f258f6b5063dff9ad
StarK-VisioN <nirajhatote1@gmail.com>
2025-08-06 23:20:41 +0530

    adding vercel file

--- backend/server.js
+++ backend/server.js
@@ -19,6 +19,7 @@ app.use(
         origin: "*",
         methods: ["GET", "POST", "PUT", "DELETE"],
         allowedHeaders: ["Content-Type", "Authorization"],
+        credentials: true,
     })
 );
 
